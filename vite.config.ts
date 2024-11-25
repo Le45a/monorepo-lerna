334797dd0d83ee35139130d5eaa5c266ec2ec085
Lee <2549720891@qq.com>
2024-11-25 11:07:26 +0800

    [chore] initialize subvue -repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // 保留符号链接，支持 PnP 的虚拟路径
-    preserveSymlinks: true,
-  },
+ 
 });
